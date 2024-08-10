9baf1854cc300493c5670d6666a59838ce1985c9
Arunjith T S <arunjith.unofficial@gmail.com>
2024-08-10 23:03:59 +0530

    added svgs that created for the website (still needs styling)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import SpacemanCanvas from './Spaceman.jsx'
 
-function Hero() {
+const Hero = ({ scrollContainer }) => {
   return (
-    <div>
-      <h1>Arunjith T S</h1>
+    <div id="hero" class="parallax">
+      <h1>Hero section</h1>
+      <img class="background" src='src/assets/parallax/background.svg' alt='' />
+      <img class="forest_line"src='src/assets/parallax/treeline.svg' alt='' />
+      <img class="mountains" src='src/assets/parallax/mountains.svg' alt='' />
+      <img class="northern_lights_1" src='src/assets/parallax/northernLights.svg' alt='' />
+      <img class="river" src='src/assets/parallax/river.svg' alt='' />
+      <img class="stars"src="src/assets/parallax/stars.svg" alt="" />
+      <img class="trees"src="src/assets/parallax/trees.svg" alt="" />
+      <SpacemanCanvas />
     </div>
   )
 }
--- src/components/Spaceman.jsx
+++ src/components/Spaceman.jsx
@@ -2,7 +2,6 @@ import { useAnimations, useGLTF } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useEffect, useRef, useState } from "react";
 import spacemanScene from "../assets/3d/spaceman.glb";
-import CanvasLoader from "./Loader";
 
 const Spaceman = ({ scale, position }) => {
   const spacemanRef = useRef();
@@ -66,7 +65,6 @@ const SpacemanCanvas = ({ scrollContainer }) => {
 
   return (
     <Canvas className={`w-full h-screen bg-transparent z-10`} camera={{ near: 0.1, far: 1000 }}>
-      <Suspense fallback={<CanvasLoader />}>
         <directionalLight position={[1, 1, 1]} intensity={2} />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 5, 10]} intensity={2} />
@@ -74,7 +72,6 @@ const SpacemanCanvas = ({ scrollContainer }) => {
         <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
         <Spaceman rotationX={rotationX} rotationY={rotationY} scale={scale} position={position} />
-      </Suspense>
     </Canvas>
   );
 };
