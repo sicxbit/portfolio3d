2fb388dc0b49a8cca41a98f2e3a485f57765551e
Arunjith T S <arunjith.unofficial@gmail.com>
2024-08-18 23:44:55 +0530

    minor style changes

--- src/components/Experiance.jsx
+++ src/components/Experiance.jsx
@@ -1,74 +1,120 @@
-import React from 'react';
-import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import React, { useEffect, useState } from 'react';
+import { motion } from "framer-motion";
 import 'react-vertical-timeline-component/style.min.css';
 import { experiences } from '../constants'; // Import experiences from your old website
 
-const ExperienceCard = ({ experience }) => {
-  return (
-    <VerticalTimelineElement
-      contentStyle={{ background: "#1d1836", color: "#fff" }}
-      contentArrowStyle={{ borderRight: "7px solid #232631" }}
-      date={experience.date}
-      icon={
-        <div
-          className=" flex justify-center items-center
-      w-full h-full"
-        >
-          <img
-            src={experience.icon}
-            alt={experience.company_name}
-            className="w-[60%] h-[60%]
-        object-contain"
-          />
-        </div>
-      }
-      iconStyle={{ background: experience.iconBg }}
-    >
-      <div>
-        <h3
-          className="text-white text-[24px]
-    font-bold"
-        >
-          {experience.title}
-        </h3>
-        <p
-          className="text-secondary text-[16px] font-semibold"
-          style={{ margin: 0 }}
-        >
-          {experience.company_name}
-        </p>
-      </div>
-      <ul className="mt-5 list-disc ml-5 space-y-2">
+const textVariant = (delay) => {
+  return {
+    hidden: {
+      y: -50,
+      opacity: 0,
+    },
+    show: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        type: "spring",
+        duration: 1.25,
+        delay: delay,
+      },
+    },
+  };
+};
+const styles = {
+  sectionText:
+    "text-white font-bold md:text-[80px] sm:text-[50px] text-[40px]",
+};
 
+const ExperienceCard = ({ experience, onClick, isActive, isMobile }) => {
+  return (
+    <div
+      onClick={onClick}
+      className={`cursor-pointer sm:mb-5 p-5 max-w-xl relative sm:text-left text-center ${isMobile ? "text-quaternary" : ""
+        }`}
+    >
+      {(isActive || isMobile) && (
+        <div className="absolute left-0 top-0 bottom-0 w-3 md:w-5 bg-tertiary my-6 sm:block hidden"></div>
+      )}
+      <h3
+        className={`text-xl lg:text-2xl xl:text-3xl font-bold sm:pl-8 ${isActive || isMobile ? "text-quaternary" : "text-slate-600"
+          }`}
+      >
+        {experience.title}
+      </h3>
+      <p
+        className={`text-md lg:text-lg xl:text-2xl sm:font-medium pt-2 sm:pl-8 ${isActive || isMobile ? "text-white" : "text-slate-600"
+          }`}
+      >
+        {experience.company_name} | {experience.date}
+      </p>
+    </div>
+  );
+};
+
+const ExperienceDetails = ({ experience }) => {
+  return (
+    <div className="mt-5">
+      <ul className="max-w-7xl list-none space-y-8 border-4 lg:border-8 rounded-xl lg:rounded-3xl p-6">
         {experience.points.map((point, index) => (
           <li
             key={`experience-point-${index}`}
-            className="text-white-100 text-[14px] pl-1 
-        tracking-wider"
+            className="text-slate-500 font-semibold text-[10px] xs:text-[14px] md:text-[18px] lg:text-[22px] xl:text-[28px] lg:leading-[30px]"
           >
             {point}
           </li>
         ))}
       </ul>
-    </VerticalTimelineElement>
-  )
+    </div>
+  );
 };
 
+
+
 const Experience = () => {
+  const [selectedJob, setSelectedJob] = useState(experiences[0]);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+
+    handleResize(); // Check initial screen size
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="experiance-container">
-      <div className='experiance'>
-        <h1>What i've done so far</h1>
-        <section className=''>
-          <VerticalTimeline>
-            {experiences.map((experience, index) => (
-              <ExperienceCard key={index} experience={experience} />
-            ))}
-          </VerticalTimeline>
-        </section>
+      <motion.div initial="hidden" animate="show" variants={textVariant(0)}>
+        <h2 className="text-white font-bold md:text-[80px] sm:text-[50px] text-[40px] text-center">
+          Experience
+        </h2>
+      </motion.div>
+
+      <div className="relative mt-10 md:mt-20 md:p-20 flex flex-col items-center sm:flex-row sm:items-start">
+        <div className="flex flex-col z-10 sm:w-auto sm:w-full">
+          {experiences.map((experience, index) => (
+            <ExperienceCard
+              key={`experience-${index}`}
+              experience={experience}
+              onClick={() => setSelectedJob(experience)}
+              isActive={selectedJob === experience}
+              isMobile={isMobile}
+            />
+          ))}
+        </div>
+
+        <div className="flex justify-end z-10 sm:block hidden">
+          <ExperienceDetails experience={selectedJob} />
+        </div>
       </div>
     </div>
   );
 };
+
 
 export default Experience;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import SpacemanCanvas from './Spaceman.jsx'
+import { ScrollContext } from './ScrollContext';
 
-const Hero = ({ scrollContainer }) => {
+const Hero = () => {
+  const { scrollY } = useContext(ScrollContext);
+  const showSpacemanAndTitle = scrollY <= 900
+  
   return (
     <div class="homepage">
       <div id="hero" class="parallax">
-        <div class="title_section">
-          <h1 class="title_header">Arunjith T S</h1>
-          <span class="title_description">
-            I make websites
-          </span>
-        </div>
+        {showSpacemanAndTitle && (
+          <div class="title_section">
+            <h1 class="title_header">Arunjith T S</h1>
+            <span class="title_description">
+              I make websites
+            </span>
+          </div>
+        )}
         <div className="background-container">
           <img src='src/assets/parallax/background.svg' alt='' />
         </div>
         <div className="forest_line-container">
           <img src='src/assets/parallax/treeline.svg' alt='' />
         </div>
-        <div className='spaceman-container'>
-          <SpacemanCanvas />
-        </div>
+        {showSpacemanAndTitle && (
+          <div className='spaceman-container'>
+            <SpacemanCanvas />
+          </div>
+        )}
         <div className="mountains-container">
           <img src='src/assets/parallax/mountains.svg' alt='' />
         </div>
