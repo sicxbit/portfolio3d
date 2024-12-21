e4eb4cea1d6e3e97f3c7ef1c4b14d8e1fd42ca43
Arunjith <arunjith.unofficial@gmail.com>
2024-12-21 21:34:18 +0530

    added code to the contact section, will update on the next commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
 function Contact() {
-    return (<div class = "contact-box">
-        <h1>Contact</h1>
-        <span>contact me</span>
-        <h1>
-            hello github
-        </h1>
-    </div>
+    return( <div class="contact-box">
+    <h1>Contact</h1>
+    <div class="socials">
+      <div class="card">
+        <h2>Kaggle</h2>
+        <a href="" target="_blank">
+          My Kaggle Profile Link
+        </a>
+      </div>
+      <div class="card">
+        <h2>GitHub</h2>
+        <a href="https://github.com/" target="_blank">
+          My GitHub Profile Link
+        </a>
+      </div>
+      </div>
+  </div>
     )
 }
 
