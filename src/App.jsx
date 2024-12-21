83fffb8f6a1dfa3038c8bb51e6679ca7aca618d6
Arunjith <arunjith.unofficial@gmail.com>
2024-12-21 14:56:24 +0530

    added fix to the overflow problem with the body

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import FullPageComponent from './components/FullPageComponent';
 import "./index.css";
 function App() {
+  useEffect(() => {
+    const setBodyHeight = () => {
+        
+        const totalHeight = document.documentElement.scrollHeight;
+        
+        document.body.style.minHeight = `${totalHeight}px`;
+    };
+    setBodyHeight();
+
+
+    window.addEventListener('resize', setBodyHeight);
+
+    return () => {
+        window.removeEventListener('resize', setBodyHeight);
+    };
+}, []);
+
   return (
     <FullPageComponent/>
   )
