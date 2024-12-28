6b7e85ff48d9bbd76746e53a775da114d53264b0
Arunjith <arunjith.unofficial@gmail.com>
2024-12-28 22:51:34 +0530

    added changes to the contact section, added svg icons

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-
+import React from 'react';
+import githubIcon from '../assets/icons/github.svg'; 
+import kaggleIcon from '../assets/icons/kaggle.svg';
 function Contact() {
-  return (<div class="contact-box">
-    <h1>Contact</h1>
-    <div class="socials">
-      <div class="card">
-        <h2>Kaggle</h2>
-        <a href="https://www.kaggle.com/arunjithts" target='_blank'>
-          My Kaggle
-        </a>
-      </div>
-      <div class="card">
-        <h2>GitHub</h2>
-        <a href="https://github.com/sicxbit" target="_blank">
-          My GitHub
-        </a>
+  return (
+    <div className="contact-box">
+      <h1>Contact</h1>
+      <div className="socials">
+        {/* Kaggle Section */}
+        <div className="card">
+          
+          <a href="https://www.kaggle.com/arunjithts" target="_blank" rel="noopener noreferrer">
+          <img
+              src={kaggleIcon}
+              alt="Kaggle Icon"
+              className="contactIcons"
+            />
+            My Kaggle
+          </a>
+        </div>
+
+        {/* GitHub Section */}
+        <div className="card">
+          <a href="https://github.com/sicxbit" target="_blank" rel="noopener noreferrer">
+            <img
+              src={githubIcon} 
+              alt="GitHub Icon"
+              className="contactIcons"
+            />
+            My GitHub
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
