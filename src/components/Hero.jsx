30c61908a9840ab16259599d97d2672b3a9ee0fc
Arunjith <arunjith.unofficial@gmail.com>
2025-01-03 23:35:15 +0530

    removed old spaceman hide feature when scrollY is 400 and fixed the stars

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,39 +5,35 @@ import background from '../assets/parallax/background.svg';
 import mountains from '../assets/parallax/mountains.svg';
 import trees from '../assets/parallax/trees.svg';
 import treeline from '../assets/parallax/treeline.svg';
-import { Stars } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
+import StarsCanvas from '../assets/canvas/Stars.jsx';
 
 const Hero = () => {
-  const { scrollY } = useContext(ScrollContext);
-  const showSpacemanAndTitle = scrollY <= 900;
+
 
   return (
 
     <div className="hero-container">
       <div className="stars">
-        <Canvas>
-          <Stars />
-        </Canvas>
+        <StarsCanvas />
       </div>
       <div className="background">
-        {showSpacemanAndTitle && (
+        
           <div className="title_section">
             <h1 className="title_header">Arunjith T S</h1>
             <span className="title_description">
               what is wrong with this borders man?
             </span>
           </div>
-        )}
+        
 
         <div className="background-container">
           <img src={background} alt="Background" />
         </div>
-        {showSpacemanAndTitle && (
+       
           <div className="spaceman-container">
             <SpacemanCanvas />
           </div>
-        )}
+       
         <div className="mountainsAndTrees-container">
           <div className="mountains-container">
             <img src={mountains} alt="Mountains" />
